c38b9b5b1cdb5cb675e65c6707571a350198d135
Joeyvolcic <36684335+Joeyvolcic@users.noreply.github.com>
2018-06-11 15:57:46 -0400

    Create ArtWork.java

---
+++ Java Labs/Unit 2/ArtWork.java
@@ -0,0 +1,45 @@
+
+public class ArtWork {
+		public static void main(String [] args){
+	
+	
+		String a="                       ___";
+		String b=" \n                 __//      |";			
+		String c=" \n                /      __ /";
+		String d="\n              |___/    ";
+		String e="\n            // | ";
+		String f="\n           ||  |/ _-__-__  ------         .......................";
+		String g="\n         //    |/          __    ||     |	                   |";
+		String h="\n         |    -------      	 ||     |                          |      ";	
+		String i="\n         |   //    //\\   //\\    || )(  |__//---\\\\_ _______ __/---\\_| ";	
+		String j="\n __-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__     ";	
+		String k="\n|\\//";	
+		String l="\n/)/";		
+		String m="\n|//";		
+		String n="\n)//";		
+				System.out.print(a);
+				System.out.print(b);
+				System.out.print(c);
+				System.out.print(d);
+				System.out.print(e);
+				System.out.print(f);
+				System.out.print(g);
+				System.out.print(h);
+				System.out.print(i);
+				System.out.print(j);
+				System.out.print(k);
+				System.out.print(l);
+				System.out.print(m);
+				System.out.print(n);
+				
+				
+				
+				
+				
+				
+				
+				
+				
+		}
+	
+}
